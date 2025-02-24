9de7cb3259209b64abb62098d33017adf898d7a5
hoa0202 <hoa0202@naver.com>
2025-02-24 14:49:28 +0900

    waitting scan data bug fix

--- launch/navigation_launch.py
+++ launch/navigation_launch.py
@@ -52,8 +52,7 @@ def generate_launch_description():
                        'behavior_server',
                        'bt_navigator',
                        'waypoint_follower',
-                       'velocity_smoother',
-                       'collision_monitor']
+                       'velocity_smoother']
 
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
     # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
@@ -88,7 +87,7 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='true',
+        default_value='false',
         description='Use simulation (Gazebo) clock if true')
 
     declare_params_file_cmd = DeclareLaunchArgument(
@@ -195,8 +194,8 @@ def generate_launch_description():
                 parameters=[configured_params],
                 arguments=['--ros-args', '--log-level', log_level],
                 remappings=remappings +
-                        # [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
-                        [('cmd_vel', 'cmd_vel_nav')]),
+                        [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
+                        # [('cmd_vel', 'cmd_vel_nav')]),
             Node(
                 package='nav2_lifecycle_manager',
                 executable='lifecycle_manager',
@@ -206,14 +205,14 @@ def generate_launch_description():
                 parameters=[{'use_sim_time': use_sim_time},
                             {'autostart': autostart},
                             {'node_names': lifecycle_nodes}]),
-            Node(
-                package='nav2_collision_monitor',
-                executable='collision_monitor',
-                output='screen',
-                emulate_tty=True,  # https://github.com/ros2/launch/issues/188
-                parameters=[configured_params],
-                remappings=remappings,
-            ),
+            # Node(
+            #     package='nav2_collision_monitor',
+            #     executable='collision_monitor',
+            #     output='screen',
+            #     emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            #     parameters=[configured_params],
+            #     remappings=remappings,
+            # ),
         ]
     )
 
@@ -263,8 +262,8 @@ def generate_launch_description():
                 name='velocity_smoother',
                 parameters=[configured_params],
                 remappings=remappings +
-                        #    [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
-                           [('cmd_vel', 'cmd_vel_nav')]),
+                           [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
+                        #    [('cmd_vel', 'cmd_vel_nav')]),
             ComposableNode(
                 package='nav2_lifecycle_manager',
                 plugin='nav2_lifecycle_manager::LifecycleManager',
@@ -272,13 +271,13 @@ def generate_launch_description():
                 parameters=[{'use_sim_time': use_sim_time,
                              'autostart': autostart,
                              'node_names': lifecycle_nodes}]),
-            ComposableNode(
-                package='nav2_collision_monitor',
-                plugin='nav2_collision_monitor::collision_monitor',
-                name='collision_monitor',
-                parameters=[configured_params],
-                remappings=remappings,
-            ),
+            # ComposableNode(
+            #     package='nav2_collision_monitor',
+            #     plugin='nav2_collision_monitor::collision_monitor',
+            #     name='collision_monitor',
+            #     parameters=[configured_params],
+            #     remappings=remappings,
+            # ),
         ],
     )
 
